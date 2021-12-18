06e984b22a10e8c2b17ba06dccbf682b210c7230
Irina Kuzmina <irina.alex.kuzmina@gmail.com>
2021-12-18 23:21:18 +0300

    1.1. HTTP и современный Web: Task1

---
+++ src/main/java/ru/netology/Constants.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+public final class Constants {
+
+    public static int NUMBER_OF_POOLS = 64;
+
+    public static final String SOURCE_PATH = "public";
+
+    public static final String SOURCE_1 = "/index.html";
+    public static final String SOURCE_2 = "/spring.svg";
+    public static final String SOURCE_3 = "/spring.png";
+    public static final String SOURCE_4 = "/resources.html";
+    public static final String SOURCE_5 = "/styles.css";
+    public static final String SOURCE_6 = "/app.js";
+    public static final String SOURCE_7 = "/links.html";
+    public static final String SOURCE_8 = "/forms.html";
+    public static final String SOURCE_9 = "/classic.html";
+    public static final String SOURCE_10 = "/events.html";
+    public static final String SOURCE_11 = "/events.js";
+
+    public static final int PORT = 9999;
+
+    public static final String ERROR_404_MESSAGE = "HTTP/1.1 404 Not Found\r\n" +
+            "Content-Length: 0\r\n" +
+            "Connection: close\r\n" +
+            "\r\n";
+
+}
---
+++ src/main/java/ru/netology/Server.java
@@ -0,0 +1,119 @@
+package ru.netology;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static ru.netology.Constants.*;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+
+    public final List<String> validPaths = List.of(SOURCE_1, SOURCE_2, SOURCE_3, SOURCE_4, SOURCE_5, SOURCE_6, SOURCE_7,
+            SOURCE_8, SOURCE_9, SOURCE_10, SOURCE_11);
+
+    public Server(int port) {
+        try {
+            this.serverSocket = new ServerSocket(port, NUMBER_OF_POOLS);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getOkMessage(String mimeType, long length) {
+        return "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: " + mimeType + "\r\n" +
+                "Content-Length: " + length + "\r\n" +
+                "Connection: close\r\n" +
+                "\r\n";
+    }
+
+    private String getErrorMessage() {
+        return ERROR_404_MESSAGE;
+    }
+
+    private boolean checkValidPath(String path, BufferedOutputStream out) throws IOException {
+        if (!validPaths.contains(path)) {
+            out.write(getErrorMessage().getBytes());
+            out.flush();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkSpecialCases(String mimeType, Path filePath, String path, BufferedOutputStream out)
+            throws IOException {
+        if (path.equals(SOURCE_9)) {
+            final var template = Files.readString(filePath);
+            final var content = template.replace(
+                    "{time}",
+                    LocalDateTime.now().toString()
+            ).getBytes();
+            out.write(getOkMessage(mimeType, content.length).getBytes());
+            out.write(content);
+            out.flush();
+            return true;
+        }
+        return false;
+    }
+
+    private void sendMessage(String mimeType, Path filePath, BufferedOutputStream out) throws IOException {
+        out.write(getOkMessage(mimeType, Files.size(filePath)).getBytes());
+        Files.copy(filePath, out);
+        out.flush();
+    }
+
+    public void runPoolOfSockets() {
+
+        final ExecutorService customerPool = Executors.newFixedThreadPool(NUMBER_OF_POOLS);
+
+        for (int i = 0; i < NUMBER_OF_POOLS; i++) {
+            customerPool.submit(this::runOneSocket);
+        }
+    }
+
+    public void runOneSocket() {
+
+        while (true) {
+            try (
+                    final var socket = serverSocket.accept();
+                    final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    final var out = new BufferedOutputStream(socket.getOutputStream())
+            ) {
+                // read only request line for simplicity
+                // must be in form GET /path HTTP/1.1
+                final var requestLine = in.readLine();
+                final var parts = requestLine.split(" ");
+
+                if (parts.length != 3) {
+                    // just close socket
+                    continue;
+                }
+
+                final var path = parts[1];
+
+                if (!checkValidPath(path, out)) continue;
+
+                final var filePath = Path.of(".", SOURCE_PATH, path);
+                final var mimeType = Files.probeContentType(filePath);
+
+                if (checkSpecialCases(mimeType, filePath, path, out)) continue;
+
+                sendMessage(mimeType, filePath, out);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
